ac13469e7d295586be04405dec6763dc28168b43
Nemohefish <64276029+Nemohefish@users.noreply.github.com>
2023-02-10 22:40:35 +0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,30 @@
+--[THE SCRIPT STILL NOT DONE]
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local UI = Material.Load({
+     Title = "Cool Script",
+     Style = 3,
+     SizeX = 450,
+     SizeY = 300,
+     Theme = "Mocha"
+})
+local Page = UI.New({
+    Title = "Main"
+})
+local Page2 = UI.New({
+    Title = "Teleport"
+
+})
+local Page3 = UI.New({
+    Title = "Player"
+})
+local Page4 = UI.New({
+    Title = "Stats"
+})
+local Page5 = UI.New({
+    Title = "Misc"
+})
+local Page6 = UI.New({
+    Title = "Some features"
+})
+
