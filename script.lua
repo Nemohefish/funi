01c5b226f6041842a25b804965cb31a114e78bf7
Nemohefish <64276029+Nemohefish@users.noreply.github.com>
2023-02-11 18:37:59 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
---[THE SCRIPT STILL NOT DONE]
+--[THE SCRIPT STILL NOT DONE
 local gameid =game.PlaceId
 if(gameid == 2753915549) then
-    print("")
+    print("Successful")
     else
     game.Players.LocalPlayer:Kick("Wrong Game")
 end
@@ -9,18 +9,20 @@ end
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 
 local UI = Material.Load({
-     Title = "Cool Script",
+     Title = "🌥Cool Script",
      Style = 3,
      SizeX = 480,
      SizeY = 330,
      Theme = "Dark"
 })
-local Page = UI.New({
-    Title = "Main"
+UI.Banner({
+    Text = "The script still in beta version"
+})
+local Page1 = UI.New({
+    Title = "Main",    
 })
 local Page2 = UI.New({
-    Title = "Teleport"
-
+    Title = "Teleport",
 })
 local Page3 = UI.New({
     Title = "Player"
@@ -33,4 +35,21 @@ local Page5 = UI.New({
 })
 local Page6 = UI.New({
     Title = "Some features"
+})
+Page2.Button({
+    Text = "Tween to start island",
+})
+Page2.Dropdown({
+    Text = "Teleport to",
+    Callback = function(value)
+        print(value)
+    end,
+    Options = {"Start Island", "Middle Island", "Banana Island", "Sandora"}
+})
+Page1.Toggle({
+    Text = "Autofarm enable",
+    Callback = function(value)
+        print(value)
+    end,
+    Enabled = false
 })
